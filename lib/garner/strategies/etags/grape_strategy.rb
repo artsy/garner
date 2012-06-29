60413a4e8f3b06d5a54e6e05c924ee9fa4eeb14a
Frank Macreery <frank@macreery.com>
2012-06-29 13:27:56 -0400

    Co-opt Grape's serialization strategy for generating ETags

--- lib/garner/strategies/etags/grape_strategy.rb
+++ lib/garner/strategies/etags/grape_strategy.rb
@@ -7,13 +7,23 @@ module Garner
           # Serialize in a way such that the ETag matches that which would
           # be returned by Grape + Rack::ETag at response time.
           def apply(object)
-            serialization = case object
-              when nil then ""
-              when String then object
-              when Hash then object.respond_to?(:to_json) ? object.to_json : MultiJson.dump(object)
-              else Marshal.dump(object)
-            end
+            serialization = encode_json(object || "")
             %("#{Digest::MD5.hexdigest(serialization)}")
+          end
+
+          # See https://github.com/intridea/grape/blob/master/lib/grape/middleware/base.rb
+          def encode_json(object)
+            return object if object.is_a?(String)
+
+            if object.respond_to? :serializable_hash
+              MultiJson.dump(object.serializable_hash)
+            elsif object.kind_of?(Array) && !object.map {|o| o.respond_to? :serializable_hash }.include?(false)
+              MultiJson.dump(object.map {|o| o.serializable_hash })
+            elsif object.respond_to? :to_json
+              object.to_json
+            else
+              MultiJson.dump(object)
+            end
           end
         end
       end
