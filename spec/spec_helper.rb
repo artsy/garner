f18ee74c197eb6c10a54bffcc18f9631ebe0a6c9
dblock <dblock@dblock.org>
2012-07-17 18:52:13 -0400

    Fixed spec, reordered includes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,10 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'rspec'
-require 'garner'
 require 'grape'
 require 'rack/test'
 require 'mongoid'
+require 'garner'
 
 [ "support/*.rb" ].each do |path|
   Dir["#{File.dirname(__FILE__)}/#{path}"].each do |file|
